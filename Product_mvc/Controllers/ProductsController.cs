15abd69b5cb173d7fda88ffa0592d57fc377e996
Ssveaj <56077830+Ssveaj@users.noreply.github.com>
2020-01-24 13:05:26 +0100

    Add project files.

---
+++ Product_mvc/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Product_mvc.Models;
+
+namespace Product_mvc.Controllers
+{
+    public class ProductsController : Controller
+    {
+        readonly List<Product> products = new List<Product>()
+        {
+            new Product {Id=1, Name="Sko 1", Price=599.99, Photo="sko1.jpg"},
+            new Product {Id=2, Name="Sko 2", Price=499.99, Photo="sko2.jpg"},
+            new Product {Id=3, Name="Sko 3", Price=578.99, Photo="sko3.jpg"}
+        };
+
+        public PartialViewResult ProtoType()
+        {
+            return PartialView();
+        }
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+    }
+}
